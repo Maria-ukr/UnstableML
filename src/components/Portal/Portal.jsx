5d6acc4310e06e5313864141f975e4efa3f5cfd5
Masianuy <Masianuy@ukr.net>
2025-04-03 18:37:08 +0300

    fixed portal

--- src/components/Portal/Portal.jsx
+++ src/components/Portal/Portal.jsx
@@ -1,9 +1,6 @@
-
 import React from 'react';
 import VideoWrap from '../VideoWrap/VideoWrap';
 import './Portal.sass';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, FreeMode, Scrollbar } from 'swiper/modules';
 
 const baseUrl = import.meta.env.BASE_URL;
 
@@ -11,87 +8,65 @@ export default function Portal() {
   return (
     <section className='portal w-full'>
       <div className='portal-wrap'>
-        <div className='portal-side portal-side__left '>
-          <Swiper
-            className='portal-slider'
-            slidesPerView={2}
-            speed={10000}
-            loop={true}
-            spaceBetween={10}
-            scrollbar={{
-              draggable: true,
-              hide: true,
-            }}
-            delay='0'
-            autoplay={{
-              delay: 0,
-              freeMode: true,
-            }}
-            modules={[Autoplay, FreeMode, Scrollbar]}
-          >
-            <SwiperSlide>
-              <VideoWrap video={`${baseUrl}videos/enhance/enhance1.mov`} classes='w-[500px] h-[350px]' />
-            </SwiperSlide>
-            <SwiperSlide>
-              <VideoWrap video={`${baseUrl}videos/enhance/enhance2.mov`} classes='w-[500px] h-[350px]' />
-            </SwiperSlide>
-            <SwiperSlide>
-              <VideoWrap
-                video={`${baseUrl}videos/enhance/enhance3.mov`}
-                classes='w-[500px] h-[350px]'
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <VideoWrap
-                video={`${baseUrl}videos/enhance/enhance4.mov`}
-                classes='w-[500px] h-[350px]'
-              />
-            </SwiperSlide>
-          </Swiper>
+        <div className='portal-side portal-side__left'>
+          <div className='portal-slider'>
+            <div className='portal-slider__track'>
+              {[...Array(2)].map((_, i) => (
+                <>
+                  <VideoWrap
+                    video={`${baseUrl}videos/enhance/enhance1.mov`}
+                    classes='w-[500px] h-[350px]'
+                    key={`v1-${i}`}
+                  />
+                  <VideoWrap
+                    video={`${baseUrl}videos/enhance/enhance2.mov`}
+                    classes='w-[500px] h-[350px]'
+                    key={`v2-${i}`}
+                  />
+                  <VideoWrap
+                    video={`${baseUrl}videos/enhance/enhance3.mov`}
+                    classes='w-[500px] h-[350px]'
+                    key={`v3-${i}`}
+                  />
+                  <VideoWrap
+                    video={`${baseUrl}videos/enhance/enhance4.mov`}
+                    classes='w-[500px] h-[350px]'
+                    key={`v4-${i}`}
+                  />
+                </>
+              ))}
+            </div>
+          </div>
         </div>
         <div className='portal-side portal-side__right'>
-          <Swiper
-            className=''
-            slidesPerView={2}
-            speed={10000}
-            loop={true}
-            spaceBetween={10}
-            scrollbar={{
-              draggable: true,
-              hide: true,
-            }}
-            autoplay={{
-              delay: '0',
-              freeMode: true,
-              reverseDirection: true,
-            }}
-            modules={[Autoplay, FreeMode, Scrollbar]}
-          >
-            <SwiperSlide>
-              <VideoWrap
-                video={`${baseUrl}videos/enhance/enhance1.mov`}
-                classes='w-[500px] h-[350px] slide'
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <VideoWrap
-                video={`${baseUrl}videos/enhance/enhance2.mov`}
-                classes='w-[500px] h-[350px] slide'
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <VideoWrap
-                video={`${baseUrl}videos/enhance/enhance3.mov`}
-                classes='w-[500px] h-[350px] slide'
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <VideoWrap
-                video={`${baseUrl}videos/enhance/enhance4.mov`}
-                classes='w-[500px] h-[350px]'
-              />
-            </SwiperSlide>
-          </Swiper>
+          <div className='portal-slider'>
+            <div className='portal-slider__track right-track'>
+              {[...Array(2)].map((_, i) => (
+                <>
+                  <VideoWrap
+                    video={`${baseUrl}videos/enhance/enhance1.mov`}
+                    classes='w-[500px] h-[350px]'
+                    key={`v1-${i}`}
+                  />
+                  <VideoWrap
+                    video={`${baseUrl}videos/enhance/enhance2.mov`}
+                    classes='w-[500px] h-[350px]'
+                    key={`v2-${i}`}
+                  />
+                  <VideoWrap
+                    video={`${baseUrl}videos/enhance/enhance3.mov`}
+                    classes='w-[500px] h-[350px]'
+                    key={`v3-${i}`}
+                  />
+                  <VideoWrap
+                    video={`${baseUrl}videos/enhance/enhance4.mov`}
+                    classes='w-[500px] h-[350px]'
+                    key={`v4-${i}`}
+                  />
+                </>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </section>
